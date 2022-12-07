31c09eee26c9ac0ea2a0f79684cdc64bff4442aa
ronigueiser <roni.gueiser@davinci.edu.ar>
2022-12-06 23:10:34 -0300

    Editar, ver y eliminar andando con modal

--- src/components/DeleteButtonModal.vue
+++ src/components/DeleteButtonModal.vue
@@ -1,15 +1,54 @@
 <script setup>
 import {ref} from "vue";
+import Modal from "./Modal.vue";
 
-defineEmits(['confirm'])
+defineEmits(['confirm', 'close'])
+// defineProps({
+//   open: {
+//     type:Boolean,
+//     default:false,
+//
+//   }
+// })
+const {open, openModal, closeModal} = useModal()
+function useModal() {
+const open = ref(false)
+  function openModal(){
+  open.value = true;
+  }
+
+  function closeModal(){
+  open.value = false
+  }
+  return {
+  open,
+    openModal,
+    closeModal
+  }
+}
+
 </script>
 
 <template>
 
-  <form action="#" method="post" @submit.prevent="() => $emit('confirm')">
+  <form action="#" method="post" @submit.prevent="() => openModal()">
 
     <button class="btn btn-danger" type="submit">Eliminar</button>
   </form>
+
+  <Modal
+  :open="open"
+  >
+    <p class="mb-3">¿Estás seguro que querés eliminar el curso?</p>
+    <div class="d-flex justify-content-end gap-2">
+
+
+      <button type="button" class="btn btn-secondary" @click="() => {closeModal(); $emit('close')}">Cancelar</button>
+
+      <button type="button" class="btn btn-danger" @click="() => {$emit('confirm'), closeModal();}">Eliminar</button>
+
+    </div>
+  </Modal>
 
 </template>
 
---
+++ src/components/Modal.vue
@@ -0,0 +1,23 @@
+
+<script setup>
+import {ref} from "vue";
+
+defineProps({
+  open: {
+    type: Boolean,
+    default: false
+  }
+});
+</script>
+
+<template>
+  <Teleport to="body">
+  <div v-if="open" class="modal-backdrop">
+
+    <div class="modal-container">
+      <slot />
+    </div>
+  </div>
+  </Teleport>
+</template>
+
